9648462cd2bb8e445cc9d1a86ea8c45cab9ebc20
Dane Herbert <116974953+sherbed968@users.noreply.github.com>
2024-09-02 15:38:46 -0500

    Update main.cpp

--- MigrationProgram/main.cpp
+++ MigrationProgram/main.cpp
@@ -1,6 +1,3 @@
-// C867project.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
 #include "degree.h"
 #include "student.h"
@@ -9,21 +6,23 @@
 
 int main()
 {
-    using namespace std;  
-    const string studentData[] = { "A1,John,Smith,John1989@gm ail.com,20,30,35,40,SECURITY",
-        "A2,Suzan,Erickson,Erickson_1990@gmailcom,19,50,30,40,NETWORK",
-        "A3,Jack,Napoli,The_lawyer99yahoo.com,19,20,40,33,SOFTWARE",
-        "A4,Erin,Black,Erin.black@comcast.net,22,50,58,40,SECURITY",
-        "A5,Dane,Herbert,dherb20@wgu.edu,24,30,30,30,SOFTWARE" };
-
+    using namespace std;
 
     cout << "c867-Scripting & Programming: Applications" << endl;
     cout << "Language: C++" << endl;
     cout << "Student ID: 010929232" << endl;
     cout << "Name: Dane Herbert" << endl;
 
+    //Declare array information
+    const string studentData[] = { "A1,John,Smith,John1989@gm ail.com,20,30,35,40,SECURITY",
+        "A2,Suzan,Erickson,Erickson_1990@gmailcom,19,50,30,40,NETWORK",
+        "A3,Jack,Napoli,The_lawyer99yahoo.com,19,20,40,33,SOFTWARE",
+        "A4,Erin,Black,Erin.black@comcast.net,22,50,58,40,SECURITY",
+        "A5,Dane,Herbert,dherb20@wgu.edu,24,30,30,30,SOFTWARE" };
+
     Roster classRoster;
 
+    //Seperate student data
     for (int i = 0; i < 5; ++i) {
         classRoster.parse(studentData[i]);
     }
@@ -44,14 +43,3 @@ int main()
 
     return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
